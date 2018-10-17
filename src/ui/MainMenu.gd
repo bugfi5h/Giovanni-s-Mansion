0821ee63030c96ca45125e2f6fc54123d7e70581
R.K <rene.kling@agentilo.com>
2018-10-17 08:41:56 +0200

    Main Menu

---
+++ src/ui/MainMenu.gd
@@ -0,0 +1,19 @@
+extends Panel
+
+var globals
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	globals = get_node("/root/globals")
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_End_pressed():
+	get_tree().quit()
+
+
+func _on_New_Game_pressed():
+	globals.goto_next_floor()
