ef09b699da8170ea3d77aa28eb82a8587062ea87
Valinor <sam.acker.business@googlemail.com>
2018-10-17 21:41:39 +0200

    made ui controllable with gamepad

--- src/ui/MainMenu.gd
+++ src/ui/MainMenu.gd
@@ -4,6 +4,8 @@ var globals
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	globals = get_node("/root/globals")
+	$"MarginContainer/HBoxContainer/VBoxContainer/MenuOptions/New Game".grab_focus()
+	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
