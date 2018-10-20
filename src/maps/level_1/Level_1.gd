7d43e330db7e6b212d5f135a413849f5dafae718
Valinor <sam.acker.business@googlemail.com>
2018-10-20 11:37:32 +0200

    lamp won't decrease during intro anymore

--- src/maps/level_1/Level_1.gd
+++ src/maps/level_1/Level_1.gd
@@ -6,12 +6,13 @@ signal let_Player_talk(message)
 var playerName = "Bob"
 var introduction_texts = [ 
 	"How the hell did I get here?",
-	"I have absolutely no idea what happened last night.",
+	"I have absolutly no idea what happened last night.",
 	"Damn alcohol! I shouldn't have trusted that cat....",
 	"...",
 	"Well let's find out what this place is. Looks a bit... shadowy..."
 ]
 var current_text = 0
+var player_lamp_decrease_temp
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,9 +24,12 @@ func start_introduction():
 	emit_signal("set_Player_inputIsEnabled", false)
 	emit_signal("let_Player_talk", playerName + ": " + introduction_texts[current_text])
 	current_text = current_text + 1
+	player_lamp_decrease_temp = $Player.lamp_decrease
+	$Player.lamp_decrease = 0
 
 func end_introduction():
 	emit_signal("set_Player_inputIsEnabled", true)
+	$Player.lamp_decrease = player_lamp_decrease_temp	
 	
 func _on_previous_animation_finished():
 	print("jo")
