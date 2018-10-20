734b30405bf512488447c1c60e713968cb6ccfb4
Rene kling <rene.kling1992@gmail.com>
2018-10-20 21:54:50 +0200

    Changed credits and added animation to level 2 fountains

--- src/maps/Map.gd
+++ src/maps/Map.gd
@@ -10,6 +10,7 @@ var bottom_limit = 0
 var globals
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimationPlayer.play("Springs")
 	globals = get_node("/root/globals")
 	set_limits()
 	set_camera_limits()
